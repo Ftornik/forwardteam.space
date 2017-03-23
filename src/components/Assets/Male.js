fe42f4a14b0092114b28039794d55b6a40838b16
Korovchenko <jenakorj@gmail.com>
2017-03-23 17:06:25 +0200

    info component

---
+++ src/components/Assets/Male.js
@@ -0,0 +1,95 @@
+import React, { Component } from 'react';
+import svg from 'lib/svg';
+
+@svg({
+    width: 600,
+    height: 600
+})
+export default class Male extends Component {
+    render = () => {
+        const st1Style = {
+            opacity: '0.5',
+            fill: '#FF9D4D'
+        };
+
+        const st2Style = {
+            opacity: '0.2',
+            fill: '#D93651'
+        };
+
+        const st3Style = {
+            fill: 'none',
+            stroke: '#3A364D',
+            strokeWidth: '8',
+            strokeLinecap: 'round',
+            strokeLinejoin: 'round',
+            strokeMiterlimit: '10'
+        };
+
+        const st8Style = {
+            opacity: '0.9',
+            fill: '#D93651'
+        };
+
+        const st9Style = {
+            fill: 'none',
+            stroke: '#FFF',
+            strokeWidth: '4',
+            strokeLinecap: 'round',
+            strokeLinejoin: 'round',
+            strokeMiterlimit: '10'
+        };
+
+        /* eslint-disable */
+        return (
+            <g id="XMLID_167_">
+                <path id="XMLID_11602_" fill={ '#FAC04A' } d="M486.5,525.5V600H296.9H107.3v-74.5c0-89.3,84.5-161.7,188.8-162h1.5
+		C402,363.8,486.5,436.2,486.5,525.5z"/>
+                <path id="XMLID_10_" style={ st1Style } d="M486.5,503.7V600H107.3v-95.3c53.8,36.7,118.8,58.2,188.8,58.2
+		C366.8,562.9,432.4,541,486.5,503.7z"/>
+                <polygon id="XMLID_11597_" style={ st2Style } points="193.4,600 193.4,510.4 225.4,600 	"/>
+                <polygon id="XMLID_11595_" style={ st2Style } points="399.4,600 399.4,510.4 367.4,600 	"/>
+                <path id="XMLID_2_" style={ st3Style } d="M107.3,600v-74.5c0-89.3,84.5-161.7,188.8-162h1.5c104.4,0.3,188.8,72.8,188.8,162V600"/>
+                <path id="XMLID_11593_" fill={ '#A6CFE6' } d="M261.6,349.3v9.2c0,19.1,15.5,34.5,34.5,34.5h0c19.1,0,34.5-15.5,34.5-34.5v-9.2"/>
+                <path id="XMLID_11589_" style={ st3Style } d="M261.6,349.3v9.2c0,19.1,15.5,34.5,34.5,34.5h0c19.1,0,34.5-15.5,34.5-34.5v-9.2"/>
+                <path id="XMLID_10463_" fill={ '#EBF1FA' } d="M296.2,354.6L296.2,354.6c-63.7,0-115.4-51.7-115.4-115.4v-65.8h230.8v65.8
+		C411.6,302.9,359.9,354.6,296.2,354.6z"/>
+                <circle id="XMLID_10454_" fill={ '#3A364D' } cx="255.9" cy="249.5" r="9.5"/>
+                <path id="XMLID_10437_" fill={ '#A6CFE6' } d="M196.5,225.3L196.5,225.3c0.2-14.9,12.4-26.9,27.3-26.9h152.9v10.4
+		c0,19.2,15.6,34.8,34.8,34.8h0v-80.4H196.7L196.5,225.3z"/>
+                <path id="XMLID_10434_" style={ st3Style } d="M221.5,229.5c9.5-12.8,30.4-18.8,47.2-14.9"/>
+                <path id="XMLID_10415_" fill={ '#3A364D' } d="M327.8,295c1.8,0,3.1,1.6,2.8,3.4c-1.1,6.4-5,12.1-10.6,16.4c-6.5,5.1-15.2,8.2-23.9,8.2
+		c-8.7,0-17.4-3.1-23.9-8.2c-5.6-4.3-9.5-10-10.6-16.4c-0.3-1.8,1.1-3.4,2.8-3.4H327.8z"/>
+                <circle id="XMLID_10402_" fill={ '#3A364D' } cx="336.4" cy="249.5" r="9.5"/>
+                <path id="XMLID_10390_" style={ st3Style } d="M274.9,500.2"/>
+                <path id="XMLID_9555_" style={ st3Style } d="M411.6,225.3v13.9c0,63.7-51.7,115.4-115.4,115.4h0c-63.7,0-115.4-51.7-115.4-115.4v-59"/>
+                <line id="XMLID_9554_" style={ st3Style } x1="193.4" y1="600" x2="193.4" y2="542.9"/>
+                <line id="XMLID_9553_" style={ st3Style } x1="193.4" y1="525.9" x2="193.4" y2="514.9"/>
+                <line id="XMLID_9552_" style={ st3Style } x1="399.4" y1="600" x2="399.4" y2="508.2"/>
+                <g id="XMLID_3188_">
+                    <path id="XMLID_9417_" fill={ '#FF5959' } d="M192.2,165.8v59.5h-9.8c-7.8,0-14-6.3-14-14v-49.2L192.2,165.8z"/>
+                    <polygon id="XMLID_8857_" style={ st8Style } points="192.2,165.8 192.2,198.6 168.4,198.6 168.4,162.1 		"/>
+                    <path id="XMLID_7959_" style={ st3Style } d="M192.2,165.8v59.5h-9.8c-7.8,0-14-6.3-14-14v-49.2L192.2,165.8z"/>
+                    <path id="XMLID_7956_" fill={ '#FF5959' } d="M209.5,17.6v22.6c0,12.5,10.1,22.6,22.6,22.6h98.8c44.5,0,80.6,36.1,80.6,80.6v81.8h-16.6
+			c-7.8,0-14-6.3-14-14v-31.1H209.5c-42.9,0-77.6-34.7-77.6-77.6v-7.4C131.8,52.3,166.6,17.6,209.5,17.6L209.5,17.6z"/>
+                    <path id="XMLID_7954_" style={ st8Style } d="M411.6,141.2v84.1h-16.6c-7.8,0-14-6.3-14-14v-31.1H209.5c-42.9,0-77.6-34.7-77.6-77.6v-7.4
+			c0-42.9,34.7-77.6,77.6-77.6l-25.7,17.8c-15.7,10.8-25,28.7-25,47.7c0,16,6.5,30.5,17,41.1c10.5,10.5,25,17,41.1,17h113.7
+			C361.4,141.2,411.6,141.2,411.6,141.2z"/>
+                    <path id="XMLID_3213_" style={ st3Style } d="M209.5,17.6v22.6c0,12.5,10.1,22.6,22.6,22.6h98.8c44.5,0,80.6,36.1,80.6,80.6v81.8h-16.6
+			c-7.8,0-14-6.3-14-14v-31.1H209.5c-42.9,0-77.6-34.7-77.6-77.6v-7.4C131.8,52.3,166.6,17.6,209.5,17.6L209.5,17.6z"/>
+                    <line id="XMLID_3211_" style={ st9Style } x1="284.1" y1="81.8" x2="254.1" y2="81.8"/>
+                    <line id="XMLID_3190_" style={ st9Style } x1="238" y1="81.8" x2="231.9" y2="81.8"/>
+                </g>
+                <path id="XMLID_3186_" style={ st3Style } d="M341.8,393.8c4.8-6.5,8.2-14,9.9-22.2"/>
+                <path id="XMLID_3184_" style={ st3Style } d="M296.2,416.8c12.9,0,24.8-4.3,34.3-11.6"/>
+                <path id="XMLID_3182_" style={ st3Style } d="M259.3,403.2c1.9,1.7,4,3.2,6.2,4.6"/>
+                <path id="XMLID_3180_" style={ st3Style } d="M240.5,370.7c1.6,8.5,5.1,16.3,10.1,23"/>
+                <path id="XMLID_3178_" style={ st3Style } d="M355.8,218c-9.9-4.4-21.8-5.8-32.2-3.4"/>
+                <path id="XMLID_3176_" style={ st3Style } d="M370.8,229.5c-1.1-1.5-2.4-2.9-3.8-4.2"/>
+                <line id="XMLID_197_" style={ st3Style } x1="296.2" y1="354.6" x2="296.2" y2="336.1"/>
+                <line id="XMLID_206_" style={ st3Style } x1="313.3" y1="353.3" x2="313.3" y2="339"/>
+                <line id="XMLID_207_" style={ st3Style } x1="279" y1="353.3" x2="279" y2="341.5"/>
+            </g>
+        );
+    }
+}
