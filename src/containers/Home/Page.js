93caa151350f378879b58b52308a5f8dfc1d2f1d
Sergey Sadovoi <serg.sadovoi@gmail.com>
2017-03-25 00:45:36 +0200

    Home page clean

--- src/containers/Home/Page.js
+++ src/containers/Home/Page.js
@@ -2,18 +2,12 @@ import React, { Component } from 'react';
 import Helmet from 'react-helmet';
 
 import Meta from './Meta';
-import * as Shared from 'components/Shared';
-import * as User from 'components/User';
 
 class HomePage extends Component {
     render = () => {
         return (
             <div>
                 <Helmet { ...Meta() }/>
-                <Shared.Header/>
-                <User.Info/>
-                <User.Sprint/>
-                <User.Pins/>
             </div>
         );
     }
