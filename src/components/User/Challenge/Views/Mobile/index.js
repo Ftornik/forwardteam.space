45fc42aeaa95f9718a9d18fc8d72c4d4be2520ce
Sergey Sadovoi <serg.sadovoi@gmail.com>
2017-04-26 23:46:04 +0300

    update: new challenge and no question

--- src/components/User/Challenge/Views/Mobile/index.js
+++ src/components/User/Challenge/Views/Mobile/index.js
@@ -23,24 +23,8 @@ function Challenge(props) {
 
     const defaultChallenges$ = [
             <p className={ reqClass }>
-                Вести молитвенный дневник в течение 21 дня<br/><br/>
-                – Этот челлендж ты можешь выполнять как с помощью тетради, так и с помощью заметок в смартфоне. Но обязательно обдумай этот момент, чтобы все твои записи сохранились.<br/><br/>
-                – Каждый день утром или вечером записывай свою молитву, разделяя ее на четыре части: Поклонение (здесь просто пиши о том, каков Бог), Исповедания (хорошая возможность попросить прощения у Него), Благодарность (ну ты понимаешь:), Ходатайство (проси о других людях, их переживаниях и радостях, о наших переживаниях и радостях, о всем). Просто пиши все то, что будет на сердце.<br/><br/>
-                Или: раздели на три таких блока: Благодарность, Прошение, Люди, которых я хочу привести к Богу.<br/><br/>
-                ВАЖНО: Записи твоего молитвенного дневника никто (кроме тебя и Бога:) читать не будет.
-            </p>,
-            <p className={ Styles.submit }>TTL – <b>22 апреля</b>. После выполнения челленджа <a href="https://goo.gl/forms/6I4CUQ3HOL47GqRL2">переходи сюда.</a></p>,
-            <p className={ reqClass }>
-                Запустить молитвенную цепочку на 14 дней<br/><br/>
-                Молитвенная цепочка - это минимум два человека, которые созваниваются каждый день для общей молитвы.<br/><br/>
-                Все очень просто: пригласи друзей, молись вместе с ними в назначенное время, получай ответы от Бога.
-            </p>,
-            <p className={ Styles.submit }>TTL – <b>20 апреля</b>. После выполнения челленджа <a href="https://goo.gl/forms/3fqesDg5g7PCxaYM2">переходи сюда</a></p>,
-            <p className={ reqClass }>
-                Написать по 7 библейских обетований на 5 жизненных ситуаций<br/><br/>
-                Жизненные ситуации: Трудности в учебе, прощение друга, поддержка человека, который тяжело болен, уважение к родителям, духовное возрождение.
-            </p>,
-            <p className={ Styles.submit }>TTL – <b>18 апреля</b>. После выполнения челленджа <a href="https://goo.gl/forms/RrECPttCGYZo7K4V2">переходи сюда</a></p>
+                Начни новый пин
+            </p>
     ];
 
     const challenges$ = user.progress.challenges ? user.progress.challenges.map((challenge) => {
@@ -68,8 +52,8 @@ function Challenge(props) {
         <section className={ Styles.challengeComponent }>
             <div className={ Styles.info }>
                 <div className={ Styles.title }>
-                    <span className={ Styles.questionMonth }>Челенджи месяца</span>&nbsp;
-                    <span className={ Styles.month }>(апрель)</span>
+                    <span className={ Styles.questionMonth }>Челендж месяца</span>&nbsp;
+                    <span className={ Styles.month }>(май)</span>
                 </div>
                 <div className={ Styles.challenges }>
                     { challenges$.length ? challenges$ : defaultChallenges$ }
